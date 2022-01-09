f736bc8640313901e32d8e725f191005310a4072
Spencer Feng <spencer.fengicloud.com>
2022-01-09 21:53:12 +1100

    feat: implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,28 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const getUrlsPromises = urls.map((url) => httpGet(url));
+
+  const results = await Promise.all(getUrlsPromises);
+
+  return results
+    .map((result) => {
+      try {
+        const message = JSON.parse(result.body).message;
+
+        if (result.status === 200) {
+          return {
+            "Arnie Quote": message,
+          };
+        }
+        return {
+          FAILURE: message,
+        };
+      } catch {
+        return null;
+      }
+    })
+    .filter((result) => result !== null);
 };
 
 module.exports = {
